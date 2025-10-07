ce814046f1a84f3bcd1434ff3f6e921d5d7fc2bb
sahnoune2 <anankarta@gmail.com>
2025-10-07 14:07:10 +0100

    config

--- back-tanit/config/config.js
+++ back-tanit/config/config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 module.exports = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://krimiseif140:VAqweZMrXclUEZCO@cluster0.nk16i.mongodb.net/TanitJobs"
+      "mongodb+srv://krimiseif140:krimiseif140@cluster0.nk16i.mongodb.net/TanitJobs"
     );
     console.log("database is connected ");
   } catch (error) {
